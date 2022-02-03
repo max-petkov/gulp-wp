bea92bc5eeafe4dd8cccc66e4133830954a0c0b0
unknown <maxy.dp@abv.bg>
2022-02-03 23:05:11 +0200

    hero-banner styling

--- src/index.php
+++ src/index.php
@@ -1,5 +1,24 @@
 <?php get_header(); ?>
 
+<section class="hero">
+    <div class="hero-container container">
+        <div class="hero-banner">
+            <h2 class="hero-banner__title">MCC Climatisation Inc.</h2>
+            <p class="hero-banner__body-text">
+                Offers you the full range of Air Conditioning and Heating services. Operating under license from the RBQ
+                and with personnel certified according to CCQ standards.
+                <br>
+                <br>
+                Located in Laval, we serve the Greater Montreal
+                area - Laval, North Shore, Longueuil, South Shore.
+            </p>
+            <button class="btn btn-primary-outline">hvac solutions</button>
+        </div>
+        <div class="hero-slider">
+            <h1>SLIDER HERE</h1>
+        </div>
+    </div>
+</section>
 
 
 <?php get_footer(); ?>
